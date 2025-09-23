f12551f773d594d41b7121abc8248cbdc266d11d
happymachine55 <hanbyeol55@naver.com>
2025-09-23 20:15:16 +0900

    비밀번호 노출 금지1

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.post('/api/ai-review', async (req, res) => {
   
 
   const prompt = makePrompt({ title: movieTitle, emotions, recommend, score });
-  const apiKey = process.env.GOOGLE_GENAI_API_KEY || "AIzaSyAn_8PrS3qcyKSYhjRY4EjCsnWaqGrY02M"; // 반드시 본인 키로 변경
+  const apiKey = process.env.GOOGLE_GENAI_API_KEY || "key"; // 반드시 본인 키로 변경
 
   try {
     const geminiRes = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + apiKey, {
